1d84fc7f080dff653c31a6addfc38184d6ec2155
seowiyeon <seowiyeon__>
2026-02-11 15:03:00 +0900

    작업 11,특정 게시물을 찾는 메서드를 구현해서 중복 제거

--- src/ll/kor/java/ssg/util/App.java
+++ src/ll/kor/java/ssg/util/App.java
@@ -61,14 +61,7 @@ public class App {
                 String[] cmdBits = cmd.split(" ");
                 int id = Integer.parseInt(cmdBits[2]);
 
-                Article foundArticle = null;
-
-                for (Article article : articles) {
-                    if (article.id == id) {
-                        foundArticle = article;
-                        break;
-                    }
-                }
+                Article foundArticle = getArticleById(id);
 
                 if (foundArticle == null) {
                     IO.println(String.format("%d번 게시물은 존재하지 않습니다.", id));
@@ -87,16 +80,7 @@ public class App {
                 String[] cmdBits = cmd.split(" ");
                 int id = Integer.parseInt(cmdBits[2]);
 
-                int foundIndex = -1;
-
-                for (int i = 0; i < articles.size(); i++) {
-                    Article article = articles.get(i);
-
-                    if (article.id == id) {
-                        foundIndex = i;
-                        break;
-                    }
-                }
+                int foundIndex = getArticleIndexById(id);
 
                 if (foundIndex == -1) {
                     IO.println(String.format("%d번 게시물은 존재하지 않습니다.", id));
@@ -110,14 +94,7 @@ public class App {
                 String[] cmdBits = cmd.split(" ");
                 int id = Integer.parseInt(cmdBits[2]);
 
-                Article foundArticle = null;
-
-                for (Article article : articles) {
-                    if (article.id == id) {
-                        foundArticle = article;
-                        break;
-                    }
-                }
+                Article foundArticle = getArticleById(id);
 
                 if (foundArticle == null) {
                     IO.println(String.format("%d번 게시물은 존재하지 않습니다.", id));
@@ -139,6 +116,28 @@ public class App {
 
         IO.println("== 프로그램 끝 ==");
         sc.close();
+    }
+
+    private int getArticleIndexById(int id) {
+        int i = 0;
+        for (Article article : articles) {
+            if (article.id == id) {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+
+    private Article getArticleById(int id) {
+        int index = getArticleIndexById(id);
+
+        if ( index != -1 ) {
+            return articles.get(index);
+        }
+
+        return null;
     }
 
     private void makeTestData() {
