01c0eb76a474e0761af97e6ecac716d95533689a
seowiyeon <weyeon06@naver.com>
2026-02-11 13:30:38 +0900

    작업 3, 게시물 저장 및 리스트 구현

---
+++ src/Article.java
@@ -0,0 +1,12 @@
+
+public class Article {
+    int id;
+    String subject;
+    String content;
+
+    public Article(int id, String subject, String content) {
+        this.id = id;
+        this.subject = subject;
+        this.content = content;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +9,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int lastArticleId = 0;
+        List<Article> articles = new ArrayList<>();
 
         while ( true ) {
             IO.print("명령어) ");
@@ -28,16 +32,28 @@ public class Main {
                 IO.print("내용 : ");
                 String content = sc.nextLine();
 
+                Article article = new Article(id, subject, content);
+                articles.add(article);
+
                 lastArticleId = id;
 
                 IO.println(String.format("%d번 글이 생성되었습니다.", id));
             } else if ( cmd.equals("list") ) {
-                IO.println("게시물이 없습니다.");
+                if ( articles.size() == 0 ) {
+                    IO.println("게시물이 없습니다.");
+                    continue;
+                }
+
+                IO.println("번호 | 제목");
+                for ( int i = articles.size() - 1; i >= 0; i-- ) {
+                    Article article = articles.get(i);
+
+                    IO.println(String.format("%d   | %s", article.id, article.subject));
+                }
             }  else {
                 IO.println("존재하지 않는 명령어입니다.");
             }
         }
-
 
         IO.println("== 프로그램 끝 ==");
         sc.close();
