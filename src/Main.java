ae7b12088ba73bdfc23577939d332d7c1db04811
seowiyeon <weyeon06@naver.com>
2026-02-11 13:27:27 +0900

    작업 1,스캐너 입력받기

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
-void main() {
+import java.util.Scanner;
 
+public class Main {
+    static void main() {
+        System.out.println("== 프로그램 시작 ==");
+        Scanner sc = new Scanner(System.in);
+
+        IO.print("명령어) ");
+        String cmd = sc.nextLine();
+        IO.println(String.format("입력된 명령어 : %s\n", cmd));
+
+
+        System.out.println("== 프로그램 끝 ==");
+        sc.close();
+    }
 }
